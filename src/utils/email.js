ac9ddbdf2becce31179ed42b683218f9a19b86fd
smx60snoop-cell <smx60snoop@gmail.com>
2025-11-28 11:11:32 +0000

    Create email.js

---
+++ src/utils/email.js
@@ -0,0 +1,7 @@
+export const sendEmail = async (to, subject, message) => {
+    // Placeholder: integrate real email provider later (e.g., Brevo, SendGrid)
+    console.log("📧 Sending email to:", to, "Subject:", subject);
+    console.log("Message:", message);
+
+    return true;
+};
