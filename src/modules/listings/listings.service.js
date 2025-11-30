eefd8e83ce6962c8d87f28841a566bb523aeb9e5
smx60snoop-cell <smx60snoop@gmail.com>
2025-11-30 09:44:23 +0000

    Update listings.service.js

--- src/modules/listings/listings.service.js
+++ src/modules/listings/listings.service.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import path from "path";
-import { db } from "../../config/db.js";
+import db from "../../config/db.js";
 import { ensureDir } from "../../utils/file.js";
 import { 
     emitListingCreated,
